72f103e05181c10e070942e583705faae175d8df
Randy Olson <olsonran@msu.edu>
2016-07-30 09:45:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,34 +3,34 @@
 from setuptools import setup, find_packages
 
 def calculate_version():
-    initpy = open('tpot/_version.py').read().split('\n')
+    initpy = open('delft/_version.py').read().split('\n')
     version = list(filter(lambda x: '__version__' in x, initpy))[0].split('\'')[1]
     return version
 
 package_version = calculate_version()
 
 setup(
-    name='TPOT',
+    name='DELFT',
     version=package_version,
-    author='Randal S. Olson',
+    author='Randal S. Olson and Rolando Garcia',
     author_email='rso@randalolson.com',
     packages=find_packages(),
-    url='https://github.com/rhiever/tpot',
+    url='https://github.com/rhiever/delft',
     license='GNU/GPLv3',
-    entry_points={'console_scripts': ['tpot=tpot:main', ]},
-    description=('Tree-based Pipeline Optimization Tool'),
+    #entry_points={'console_scripts': ['tpot=tpot:main', ]},
+    description=('A Python tool that automatically optimizes deep learning pipelines using genetic programming. '),
     long_description='''
-A Python tool that automatically creates and optimizes machine learning pipelines using genetic programming.
+A Python tool that automatically optimizes deep learning pipelines using genetic programming. 
 
 Contact
 =============
-If you have any questions or comments about TPOT, please feel free to contact me via:
+If you have any questions or comments about DELFT, please feel free to contact me via:
 
 E-mail: rso@randalolson.com
 
 or Twitter: https://twitter.com/randal_olson
 
-This project is hosted at https://github.com/rhiever/tpot
+This project is hosted at https://github.com/rhiever/delft
 ''',
     zip_safe=True,
     install_requires=['numpy', 'scipy', 'pandas', 'scikit-learn', 'deap', 'update_checker', 'tqdm'],
@@ -44,5 +44,5 @@ This project is hosted at https://github.com/rhiever/tpot
         'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering :: Artificial Intelligence'
     ],
-    keywords=['pipeline optimization', 'hyperparameter optimization', 'data science', 'machine learning', 'genetic programming', 'evolutionary computation'],
+    keywords=['deep learning', 'autoencoder', 'pipeline optimization', 'hyperparameter optimization', 'data science', 'machine learning', 'genetic programming', 'evolutionary computation'],
 )
